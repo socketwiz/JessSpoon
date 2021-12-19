ced16441a31515eeb47aefed034737705deb34e6
Ricky Nelson <rickyn@socketwiz.com>
2021-12-19 17:17:30 -0500

    converting to gatsby

---
+++ content/home/index.md
@@ -0,0 +1,7 @@
+---
+title: "Home"
+date: 2021-02-26T22:15:36-05:00
+draft: true
+---
+
+JessSpoon
---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    purge: [
+        "./templates/**/*.html"
+    ],
+    darkMode: 'class', // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+    ],
+}
